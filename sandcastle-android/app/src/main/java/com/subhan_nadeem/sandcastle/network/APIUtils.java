062dc85d2c47eece5b964fbafa3c656add4662e8
Subhan Nadeem <subhan97@hotmail.com>
2018-05-14 15:17:27 -0400

    Updated EC2 link

--- sandcastle-android/app/src/main/java/com/subhan_nadeem/sandcastle/network/APIUtils.java
+++ sandcastle-android/app/src/main/java/com/subhan_nadeem/sandcastle/network/APIUtils.java
@@ -4,7 +4,7 @@ package com.subhan_nadeem.sandcastle.network;
  * Created by Subhan Nadeem on 2017-10-10.
  */
 public class APIUtils {
-    public static final String BASE_URL = "http://ec2-13-59-203-250.us-east-2.compute.amazonaws.com";
+    public static final String BASE_URL = "http://ec2-18-191-55-141.us-east-2.compute.amazonaws.com";
     //  private static final String BASE_URL = "http://localhost:3000";
 
     public static APIService getAPIService() {
