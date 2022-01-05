bd2da2d3deded07244c61284f530ffb15363dbf7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-05 23:19:30 +0800

    Delete sandcastle-android directory

--- sandcastle-android/app/src/main/java/com/subhan_nadeem/sandcastle/features/maps/OnNearbyUsersUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package com.subhan_nadeem.sandcastle.features.maps;
-
-/**
- * Created by Subhan Nadeem on 2017-10-28.
- */
-
-interface OnNearbyUsersUpdateListener {
-    void onNearbyUsersUpdated(int numUsersNearby);
-}
--- sandcastle-android/app/src/main/java/com/subhan_nadeem/sandcastle/features/room/RoomViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.subhan_nadeem.sandcastle.features.room;
-
-import android.arch.lifecycle.ViewModel;
-
-public class RoomViewModel extends ViewModel {
-
-}
